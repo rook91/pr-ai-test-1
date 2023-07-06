5509adbf70859cd28b18ddfef5244f21b7d76908
rook01 <tomasz.gawron@outlook.com>
2023-07-06 22:43:37 +0200

    test endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import express from "express";
 import { getAllFixtures, getOddsByFixture } from './request.js';
 // import { SP_Fixture, SP_OddsByFixture } from './types.js'
 
@@ -7,7 +8,23 @@ const allFixtures = await getAllFixtures();
 const fixturesCount = allFixtures.length;
 
 for(let i = 0; i<fixturesCount; i++){
-  console.log(`${allFixtures[i].id}: ${allFixtures[i].name}`);
+  // console.log(`${allFixtures[i].id}: ${allFixtures[i].name}`);
   const allOdds = await getOddsByFixture(allFixtures[i].id);
   console.log(allOdds);
 }
+
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log(req);
+  res.send('Test endpoint');
+});
+
+app.get('/allFixtures', (req, res) => {
+  console.log(req);
+  res.send(allFixtures);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
