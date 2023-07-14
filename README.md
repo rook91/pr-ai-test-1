b201e2c8af900f31435c79cedfaafee6263b559d
rook01 <tomasz.gawron@outlook.com>
2023-07-14 18:16:42 +0200

    Secrets update

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # pr-ai-test-1
+
+
+#### Prerequisite
+
+Before you start make sure you have installed:
+- [Azure Developer CLI](https://learn.microsoft.com/en-us/azure/developer/azure-developer-cli/install-azd?tabs=winget-windows,brew-mac,script-linux&pivots=os-windows)
+
+- [Azure Acoount extension](https://github.com/Microsoft/vscode-azure-account)
+
+
+#### How to run
+
+1. Run in terminal `azd auth login`.
+2. Run npm run start
--- src/sectrets.ts
+++ src/sectrets.ts
@@ -13,6 +13,8 @@ export const testSecrets = async () => {
   // - AZURE_CLIENT_ID: The application (client) ID registered in the AAD tenant
   // - AZURE_CLIENT_SECRET: The client secret for the registered application
 
+  // console.log("secrets------------");
+
   const credential = new DefaultAzureCredential();
 
   // const credential = new VisualStudioCodeCredential();
@@ -26,27 +28,31 @@ export const testSecrets = async () => {
   // The secret can be a string of any kind. For example,
   // a multiline text block such as an RSA private key with newline characters,
   // or a stringified JSON object, like `JSON.stringify({ mySecret: 'MySecretValue'})`.
-  const uniqueString = new Date().getTime();
-  const secretName = `secret${uniqueString}`;
-  const result = await client.setSecret(secretName, "MySecretValue");
-  console.log("result: ", result);
+  // const uniqueString = new Date().getTime();
+  // const secretName = `secret${uniqueString}`;
+  // const result = await client.setSecret(secretName, "MySecretValue");
+  // console.log("result: ", result);
 
-  // Read the secret we created
-  const secret = await client.getSecret(secretName);
-  console.log("secret: ", secret);
+  // // Read the secret we created
+  // const secret = await client.getSecret(secretName);
+  // console.log("secret: ", secret);
 
   const secret2 = await client.getSecret(SECRET_NAME);
-  console.log("API TOKEN: ", secret2);
+  // console.log("API TOKEN: ", secret2);
 
   // Update the secret with different attributes
-  const updatedSecret = await client.updateSecretProperties(secretName, result.properties.version, {
-    enabled: false
-  });
-  console.log("updated secret: ", updatedSecret);
+  // const updatedSecret = await client.updateSecretProperties(secretName, result.properties.version, {
+  //   enabled: false
+  // });
+  // console.log("updated secret: ", updatedSecret);
 
   // Delete the secret immediately without ability to restore or purge.
-  await client.beginDeleteSecret(secretName);
+  // await client.beginDeleteSecret(secretName);
 
-  console.log('Return value')
-  return 'ALA>MA>KOTA';
+  return getSecretValue(secret2);
+}
+
+const getSecretValue = (secret) => {
+  const secVal = secret ? secret.value : '';
+  return secVal;
 }
